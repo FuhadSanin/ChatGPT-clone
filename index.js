651c2030260147a2b1bce8313c9e63729ad591a7
FuhadSanin <fuhadsanin500@gmail.com>
2023-03-12 04:06:09 -0700

    Backend integrated

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
 //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
 
 const { Configuration, OpenAIApi } = require("openai");
+const bodyparser = require('body-parser')
+const cors = require('cors')
 const express = require('express')
 const configuration = new Configuration({
     organization: "org-vgSn4MgXnJwtxVxFZcjKFFSR",
-    apiKey: "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",
+    apiKey: "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",
 });
 
 
 
 const app = express()
-const port = 3001
-
-async function callapi(){
-    const openai = new OpenAIApi(configuration);
-    const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: "What is your name",
-    max_tokens: 7,
-    temperature: 0,
-    });
-    console.log(response.data.choices[0].text)
-}
-callapi()
+app.use(bodyparser.json())
+app.use(cors())
+const port = 3000
 
 app.post('/',async(req,res)=>{
+    const {message}=req.body
+    console.log(message,"message")
     const openai = new OpenAIApi(configuration);
     const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: "What is your name",
-    max_tokens: 7,
-    temperature: 0,
+    prompt: `${message}`,
+    max_tokens: 100,
+    temperature: 0.5,
     });
     console.log(response.data.choices[0].text)
+    res.json({
+        // data:response.data
+        message:response.data.choices[0].text
+
+    })
 })
 
 app.listen(port,()=>{
-    console.log(`Example ${port}`)
+    console.log(`Listening on http://localhost:${port}/`)
 })
--- src/App.js
+++ src/App.js
@@ -3,32 +3,46 @@ import React,{useState} from 'react';
 
 function App() {
   const [input,setInput] = useState("");
-  const [chatlog,setChatlog] = useState([{
-    user:"gpt",
-    message:"How can I help you ?"
-  },{
-      user:"me",
-      message:"I need to know about whether ?"
-  }])
+  const [chatlog,setChatlog] = useState([])
+
+  function clear(){
+    setChatlog([])
+  }
 
   async function handleSubmit(e) {
     e.preventDefault()
-    setInput("")
-    setChatlog([...chatlog,{user:"me" , message:`${input}`}])
+    await setInput("")
+    let chatlogNew = [...chatlog,{user:"me" , message:`${input}`}]
+    setChatlog(chatlogNew)
     console.log("submit")
     console.log(`${input}`)
+
+    //sending the chtlog message to api
+    const messges = chatlogNew.map((message)=>message.message).join("\n")
+    const response = await fetch("http://localhost:3000/",{
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body:JSON.stringify({
+      message:messges
+    })
+    })
+    const data = await response.json()
+    await setChatlog([...chatlogNew,{user:"gpt",message:`${data.message}`}])
+    console.log(data.message)
+
   }
   return (
     <div className="App">
       <div className="left-menu">
-        <div className='new-chat'><span>+</span>New Chat</div>
+        <div onClick={clear} className='new-chat'><span>+</span>New Chat</div>
       </div>
 
       <div className='right-menu'>
         <div className='chatbox'>
           <form onSubmit={handleSubmit}>
             <input value={input} onChange={(e)=>setInput(e.target.value)} placeholder='Type your message here' className='chat-input-feild' />
-
           </form>
         </div>
 
@@ -44,7 +58,6 @@ const ChatMessage=({message})=>{
    <div className={`chat-log ${message.user === "gpt" && "chatgpt"}`}>
       <div className="chat-message">
         <div className={`avatar ${message.user === "gpt" && "chatgpt"}`}>
-          {` ${message.user === "gpt" && <img src={require('./image/openai-avatar.png')} alt="" />}`}
 
         </div>
         <div className='message'>
